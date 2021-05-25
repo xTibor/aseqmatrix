c68a6e19d9446c6e56163216f378f62ab4068d4a
Nagy Tibor <xnagytibor@protonmail.com>
2021-05-25 11:21:40 +0200

    Interface work with mock data

--- src/main.rs
+++ src/main.rs
@@ -148,14 +148,60 @@ fn main() -> Result<(), String> {
 
     canvas.set_draw_color(pixels::Color::RGB(128, 128, 128));
     canvas.clear();
-    draw_string(&mut canvas, &texture, "Hello, world!", (10, 10), 0)?;
-    draw_string(&mut canvas, &texture, "Hello, world!", (10, 10), 1)?;
-    draw_string(&mut canvas, &texture, "Hello, world!", (10, 10), 2)?;
-    draw_string(&mut canvas, &texture, "Hello, world!", (10, 10), 3)?;
 
-    draw_tiles(&mut canvas, &texture, (0, 0), (10, 0), 2, 2)?;
-    draw_tiles(&mut canvas, &texture, (3, 0), (12, 0), 2, 2)?;
-    draw_tiles(&mut canvas, &texture, (6, 0), (14, 0), 2, 2)?;
+    let inputs = [
+        "Timer",
+        "Announce",
+        "Midi Through Port-0",
+        "VirMIDI 3-0",
+        "VirMIDI 3-1",
+        "VirMIDI 3-2",
+        "VirMIDI 3-3",
+    ];
+    let outputs = [
+        "Midi Through Port-0",
+        "VirMIDI 3-0",
+        "VirMIDI 3-1",
+        "VirMIDI 3-2",
+        "VirMIDI 3-3",
+        "TiMidity port 0",
+        "TiMidity port 1",
+        "TiMidity port 2",
+        "TiMidity port 3",
+        "BinaryPiano2",
+        "aseqdump",
+    ];
+
+    {
+        let window_width = (1 + inputs.len() * 2 + 1 + outputs.iter().map(|s| s.len()).max().unwrap_or(0) + 2) * TILE_SIZE;
+        let window_height = (1 + outputs.len() * 2 + 1 + inputs.iter().map(|s| s.len()).max().unwrap_or(0) + 2) * TILE_SIZE;
+        let window = canvas.window_mut();
+        window.set_size(window_width as u32, window_height as u32).map_err(|e| e.to_string())?;
+    }
+
+    for (output_index, output) in outputs.iter().enumerate() {
+        let y = 1 + output_index as isize * 2;
+        let x_arrow_right = inputs.len() as isize * 2 + 1;
+        let x_text = x_arrow_right + 2;
+
+        draw_tiles(&mut canvas, &texture, (2, 0), (x_arrow_right, y), 1, 2)?;
+        draw_string(&mut canvas, &texture, output, (x_text, y), 0)?;
+    }
+
+    for (input_index, input) in inputs.iter().enumerate() {
+        let x = 1 + input_index as isize * 2;
+        let y_arrow_down = outputs.len() as isize * 2 + 1;
+        let y_text = y_arrow_down + input.len() as isize + 1;
+
+        draw_tiles(&mut canvas, &texture, (0, 3), (x, y_arrow_down), 2, 1)?;
+        draw_string(&mut canvas, &texture, input, (x, y_text), 3)?;
+    }
+
+    for y in 0..outputs.len() {
+        for x in 0..inputs.len() {
+            draw_tiles(&mut canvas, &texture, (0, 0), (1 + x as isize * 2, 1 + y as isize * 2), 2, 2)?;
+        }
+    }
 
     canvas.present();
 
